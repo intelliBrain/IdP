b4661551de1b29c7d2c87377a89b5254f5836251
Rene Hanslin <rene.hanslin@digitecgalaxus.ch>
2018-11-27 10:03:28 +0100

    IdP.SimpleClient initial commit

---
+++ IdP.SimpleClient/Pages/About.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace IdP.SimpleClient.Pages
+{
+    [Authorize]
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+
+        public async Task OnGetLogoff()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+    }
+}
